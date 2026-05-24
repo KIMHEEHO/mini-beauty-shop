72970016e57b762a9d3d0cfe2edb0ec312f0add5
희호 <kim21670@naver.com>
2026-05-24 23:02:49 +0900

    chore: main.tsx, App.tsx 타입스크립트 컴파일 에러 해결 및 세팅 완료

---
+++ src/vite-env.d.ts
@@ -0,0 +1,20 @@
+// src/declarations.d.ts
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
